059b7443401beb8c23cebf4832e4a90102c90e91
Arsenii Lyzenko <arseniysmart@gmail.com>
2019-06-07 20:23:46 +0300

    Update index.js

--- HammingDistance/index.js
+++ HammingDistance/index.js
@@ -4,7 +4,7 @@ The Hamming distance between two integers is the number of positions at which th
 Given two integers x and y, calculate the Hamming distance.
 
 Note:
-0 ≤ x, y < 231.
+0 ≤ x, y < 2^31.
 
 Example:
 
