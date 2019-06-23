c3b630951f1844ec472afaea4b2e25a3e6994c65
Arsenii <arseniysmart@gmail.com>
2019-06-23 15:33:44 +0300

    init

---
+++ ReverseLinkedList/index.js
@@ -0,0 +1,22 @@
+/*
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+
+Follow up:
+
+A linked list can be reversed either iteratively or recursively.
+Could you implement both?
+*/
+
+function reverseLinkedList_iter(linkedList) {
+
+}
+
+function ListNode(val, next = null) {
+  this.val = val;
+  this.next = next;
+}
---
+++ ReverseLinkedList/tests.js
@@ -0,0 +1,37 @@
+describe("reverseLinkedList_iter()", function() {
+  it("1->2->3->4->5->NULL → 5->4->3->2->1->NULL", function() {
+    assert.deepEqual(reverseLinkedList_iter(
+      new ListNode(1,
+        new ListNode(2,
+          new ListNode(3,
+            new ListNode(4,
+              new ListNode(5)))))),
+      new ListNode(5,
+        new ListNode(4,
+          new ListNode(3,
+            new ListNode(2,
+              new ListNode(1))))));
+  });
+
+  it("1->NULL → 1->NULL", function() {
+    assert.deepEqual(reverseLinkedList_iter(
+      new ListNode(1)),
+      new ListNode(1));
+  });
+
+  it("1->1->1->2->NULL → 2->1->1->1->NULL", function() {
+    assert.deepEqual(reverseLinkedList_iter(
+      new ListNode(1,
+        new ListNode(1,
+          new ListNode(1,
+            new ListNode(2))))),
+      new ListNode(2,
+        new ListNode(1,
+          new ListNode(1,
+            new ListNode(1)))));
+  });
+
+  it("NULL → NULL", function() {
+    assert.deepEqual(reverseLinkedList_iter(null), null);
+  });
+});
