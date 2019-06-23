933ac90223c64c1346f2e7299fb609316d2a6fa3
Arsenii <arseniysmart@gmail.com>
2019-06-23 15:38:11 +0300

    solved iteratively: time O(N), space O(N)

--- ReverseLinkedList/index.js
+++ ReverseLinkedList/index.js
@@ -13,10 +13,39 @@ Could you implement both?
 */
 
 function reverseLinkedList_iter(linkedList) {
+  if (!linkedList) return null;
 
+  linkedList = listToArray(linkedList);
+  linkedList = linkedList.reverse();
+  linkedList = arrayToList(linkedList);
+
+  return linkedList;
 }
 
 function ListNode(val, next = null) {
   this.val = val;
   this.next = next;
+}
+
+function listToArray(list) {
+  let arr = [];
+  let current = list;
+  while (current.next) {
+    arr.push(current.val);
+    current = current.next;
+  }
+  arr.push(current.val);
+
+  return arr;
+}
+
+function arrayToList(arr) {
+  let list;
+  let nextRest;
+  for (let i = arr.length - 1; i >= 0; --i) {
+    nextRest = list || null;
+    list = new ListNode(arr[i], nextRest);
+  }
+
+  return list;
 }
