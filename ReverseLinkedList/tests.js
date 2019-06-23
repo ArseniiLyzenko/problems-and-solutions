90c5800b941143ef6798660bfc9fd2a32de5dc34
Arsenii <arseniysmart@gmail.com>
2019-06-23 20:52:03 +0300

    Added tests for recursive version of the func

--- ReverseLinkedList/tests.js
+++ ReverseLinkedList/tests.js
@@ -35,3 +35,41 @@ describe("reverseLinkedList_iter()", function() {
     assert.deepEqual(reverseLinkedList_iter(null), null);
   });
 });
+
+describe("reverseLinkedList_rec()", function() {
+  it("1->2->3->4->5->NULL → 5->4->3->2->1->NULL", function() {
+    assert.deepEqual(reverseLinkedList_rec(
+      new ListNode(1,
+        new ListNode(2,
+          new ListNode(3,
+            new ListNode(4,
+              new ListNode(5)))))),
+      new ListNode(5,
+        new ListNode(4,
+          new ListNode(3,
+            new ListNode(2,
+              new ListNode(1))))));
+  });
+
+  it("1->NULL → 1->NULL", function() {
+    assert.deepEqual(reverseLinkedList_rec(
+      new ListNode(1)),
+      new ListNode(1));
+  });
+
+  it("1->1->1->2->NULL → 2->1->1->1->NULL", function() {
+    assert.deepEqual(reverseLinkedList_rec(
+      new ListNode(1,
+        new ListNode(1,
+          new ListNode(1,
+            new ListNode(2))))),
+      new ListNode(2,
+        new ListNode(1,
+          new ListNode(1,
+            new ListNode(1)))));
+  });
+
+  it("NULL → NULL", function() {
+    assert.deepEqual(reverseLinkedList_rec(null), null);
+  });
+});
