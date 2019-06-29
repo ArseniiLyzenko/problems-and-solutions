240c871d812d8170af87a97c069eb7151a7bc4f3
Arsenii <arseniysmart@gmail.com>
2019-06-29 13:26:51 +0300

    Added some tests

--- MergeTwoSortedLists/tests.js
+++ MergeTwoSortedLists/tests.js
@@ -54,4 +54,46 @@ describe("mergeTwoSortedLists()", function() {
 
     assert.deepEqual(mergeTwoSortedLists(...input), output);
   });
+
+  it("1->3->5, 2 → 1->2->3->5", function() {
+    let input = [
+      new ListNode(1,
+        new ListNode(3,
+          new ListNode(5)
+        )
+      ),
+      new ListNode(2)
+    ];
+    let output = new ListNode(1,
+                  new ListNode(2,
+                    new ListNode(3,
+                      new ListNode(5,
+                      )
+                    )
+                  )
+                );
+
+    assert.deepEqual(mergeTwoSortedLists(...input), output);
+  });
+
+  it("4, 1->3->5 → 1->3->4->5", function() {
+    let input = [
+      new ListNode(4),
+      new ListNode(1,
+        new ListNode(3,
+          new ListNode(5)
+        )
+      )
+    ];
+    let output = new ListNode(1,
+                  new ListNode(3,
+                    new ListNode(4,
+                      new ListNode(5,
+                      )
+                    )
+                  )
+                );
+
+    assert.deepEqual(mergeTwoSortedLists(...input), output);
+  });
 });
