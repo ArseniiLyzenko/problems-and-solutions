ac1c29a588560dae8fcf6f6b1947b915b9ee4286
Arsenii <arseniysmart@gmail.com>
2019-06-29 14:16:27 +0300

    solved: time O(N), space O(1)

--- MergeTwoSortedLists/index.js
+++ MergeTwoSortedLists/index.js
@@ -9,9 +9,34 @@ Output: 1->1->2->3->4->4
 */
 
 function mergeTwoSortedLists(list1, list2) {
+  if (!list1 && !list2) return null;
+  if (!list1) return list2;
+  if (!list2) return list1;
+
   let list = null;
+  let cur = null;
 
+  if (list1.val < list2.val) {
+    list = list1;
+    cur = list1;
+    list1 = list1.next;
+  } else {
+    list = list2;
+    cur = list2;
+    list2 = list2.next;
+  }
 
+  while (list1 || list2) {
+    if (((list1) ? list1.val : Infinity) < ((list2) ? list2.val : Infinity)) {
+      cur.next = list1;
+      cur = cur.next;
+      list1 = list1.next;
+    } else {
+      cur.next = list2;
+      cur = cur.next;
+      list2 = list2.next;
+    }
+  }
 
   return list;
 }
