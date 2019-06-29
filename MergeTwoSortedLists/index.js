94fcc40baea6fe7786d1b0dc833897e54c8ec204
Arsenii <arseniysmart@gmail.com>
2019-06-29 13:15:57 +0300

    init

---
+++ MergeTwoSortedLists/index.js
@@ -0,0 +1,22 @@
+/*
+Merge two sorted linked lists and return it as a new list. The new list
+should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+*/
+
+function mergeTwoSortedLists(list1, list2) {
+  let list = null;
+
+
+
+  return list;
+}
+
+function ListNode(val, next = null) {
+  this.val = val;
+  this.next = next;
+}
---
+++ MergeTwoSortedLists/tests.js
@@ -0,0 +1,57 @@
+describe("mergeTwoSortedLists()", function() {
+  it("1->2->4, 1->3->4 → 1->1->2->3->4->4", function() {
+    let input = [
+      new ListNode(1,
+        new ListNode(2,
+          new ListNode(4)
+      )
+    ),
+      new ListNode(1,
+        new ListNode(3,
+          new ListNode(4)
+        )
+      )
+    ];
+    let output = new ListNode(1,
+                    new ListNode(1,
+                      new ListNode(2,
+                        new ListNode(3,
+                          new ListNode(4,
+                            new ListNode(4)
+                          )
+                        )
+                      )
+                    )
+                  );
+
+    assert.deepEqual(mergeTwoSortedLists(...input), output);
+  });
+
+  it("1->3->5, 2->4->6 → 1->2->3->4->5->6", function() {
+    let input = [
+      new ListNode(1,
+        new ListNode(3,
+          new ListNode(5)
+        )
+      ),
+      new ListNode(2,
+        new ListNode(4,
+          new ListNode(6)
+        )
+      )
+    ];
+    let output = new ListNode(1,
+                  new ListNode(2,
+                    new ListNode(3,
+                      new ListNode(4,
+                        new ListNode(5,
+                          new ListNode(6)
+                        )
+                      )
+                    )
+                  )
+                );
+
+    assert.deepEqual(mergeTwoSortedLists(...input), output);
+  });
+});
