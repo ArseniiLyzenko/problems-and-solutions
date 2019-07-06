4e1afb920a5ed59636edd13673d81804b7c4a4c5
Arsenii <arseniysmart@gmail.com>
2019-07-06 15:40:21 +0300

    mistype fix

--- TwoSum/tests.js
+++ TwoSum/tests.js
@@ -3,11 +3,11 @@ describe("twoSum()", function() {
     assert.deepEqual(twoSum([2, 7, 11, 15], 9), [0, 1]);
   });
 
-  it("[2, 7, 11, 15], 17 → [0, 1]", function() {
+  it("[2, 7, 11, 15], 17 → [0, 3]", function() {
     assert.deepEqual(twoSum([2, 7, 11, 15], 17), [0, 3]);
   });
 
-  it("[2, 7, 11, 15], 18 → [0, 1]", function() {
+  it("[2, 7, 11, 15], 18 → [1, 2]", function() {
     assert.deepEqual(twoSum([2, 7, 11, 15], 18), [1, 2]);
   });
 });
