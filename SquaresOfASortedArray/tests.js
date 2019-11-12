fa8cc5fcd9779d125630c57c4b731b69259064c4
Arsenii <arseniysmart@gmail.com>
2019-11-12 18:19:58 -0500

    init

---
+++ SquaresOfASortedArray/tests.js
@@ -0,0 +1,16 @@
+let input;
+let output;
+
+describe("sortedSquares()", function() {
+  input = [-4,-1,0,3,10];
+  output = [0,1,9,16,100];
+  it(`${input} → ${output}`, function() {
+    assert.equal(sortedSquares(input), output);
+  });
+
+  input = [-7,-3,2,3,11];
+  output = [4,9,9,49,121];
+  it(`${input} → ${output}`, function() {
+    assert.equal(sortedSquares(input), output);
+  });
+});
