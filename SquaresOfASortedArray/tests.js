24ec769735dab695159cca5371c14d942af61fe1
Arsenii <arseniysmart@gmail.com>
2019-11-14 11:50:46 -0500

    Fixed: comparing arrays

--- SquaresOfASortedArray/tests.js
+++ SquaresOfASortedArray/tests.js
@@ -5,12 +5,12 @@ describe("sortedSquares()", function() {
   input = [-4,-1,0,3,10];
   output = [0,1,9,16,100];
   it(`${input} → ${output}`, function() {
-    assert.equal(sortedSquares(input), output);
+    assert.deepEqual(sortedSquares(input), output);
   });
 
   input = [-7,-3,2,3,11];
   output = [4,9,9,49,121];
   it(`${input} → ${output}`, function() {
-    assert.equal(sortedSquares(input), output);
+    assert.deepEqual(sortedSquares(input), output);
   });
 });
