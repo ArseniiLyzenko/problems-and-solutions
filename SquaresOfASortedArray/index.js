3bd90bbddb5fa5b918a83a1be620b246bca0a49c
Arsenii <arseniysmart@gmail.com>
2019-11-15 13:59:40 -0500

    Solved: time O(N), space O(1)

--- SquaresOfASortedArray/index.js
+++ SquaresOfASortedArray/index.js
@@ -16,6 +16,21 @@ Note:
   A is sorted in non-decreasing order.
 */
 
-function sortedSquares() {
+function sortedSquares(A) {
+  let squares = [];
 
+  let pointer1 = 0;
+  let pointer2 = A.length - 1;
+
+  for (let i = 0, l = A.length; i < l; i++) {
+    if (Math.abs(A[pointer1]) > Math.abs(A[pointer2])) {
+      squares.push(A[pointer1]**2);
+      pointer1++;
+    } else {
+      squares.push(A[pointer2]**2);
+      pointer2--;
+    }
+  }
+
+  return squares.reverse();
 }
