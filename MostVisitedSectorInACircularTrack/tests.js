46b605d8a38c7573965155ba0e5b17acd76a4005
ArseniiLyzenko <arseniysmart@gmail.com>
2020-08-25 12:45:47 -0400

    init

---
+++ MostVisitedSectorInACircularTrack/tests.js
@@ -0,0 +1,10 @@
+let input;
+let output;
+
+describe("mostVisited()", function() {
+  input = null;
+  output = null;
+  it(`${input} → ${output}`, function() {
+    assert.equal(mostVisited(input), output);
+  });
+});
