408f5f8eb39372e300a62171d54b454444a3379f
ArseniiLyzenko <arseniysmart@gmail.com>
2020-09-15 07:05:40 -0400

    Added: 3 test cases

--- MostVisitedSectorInACircularTrack/tests.js
+++ MostVisitedSectorInACircularTrack/tests.js
@@ -1,10 +1,13 @@
-let input;
-let output;
-
 describe("mostVisited()", function() {
-  input = null;
-  output = null;
-  it(`${input} → ${output}`, function() {
-    assert.equal(mostVisited(input), output);
+  it(`4, [1,3,1,2] → [1,2]`, function() {
+    assert.deepEqual(mostVisited(4, [1,3,1,2]), [1,2]);
+  });
+
+  it(`2, [2,1,2,1,2,1,2,1,2] → [2]`, function() {
+    assert.deepEqual(mostVisited(2, [2,1,2,1,2,1,2,1,2]), [2]);
+  });
+
+  it(`7, [1,3,5,7] → [1,2,3,4,5,6,7]`, function() {
+    assert.deepEqual(mostVisited(7, [1,3,5,7]), [1,2,3,4,5,6,7]);
   });
 });
