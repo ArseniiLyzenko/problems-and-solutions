3f16085f1b1c6d97b4106ccec8ff25eab7d8aa21
ArseniiLyzenko <arseniysmart@gmail.com>
2020-09-24 19:39:45 +0300

    init

---
+++ RomanToInteger/tests.js
@@ -0,0 +1,10 @@
+let input;
+let output;
+
+describe("funcName()", function() {
+  input = null;
+  output = null;
+  it(`${input} → ${output}`, function() {
+    assert.equal(funcName(input), output);
+  });
+});
