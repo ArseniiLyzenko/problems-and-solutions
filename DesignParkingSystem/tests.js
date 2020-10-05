46b6e56de041d34330ce863ae17fa138c8f87ade
ArseniiLyzenko <arseniysmart@gmail.com>
2020-10-05 17:10:25 +0300

    Added: 3 test-cases

--- DesignParkingSystem/tests.js
+++ DesignParkingSystem/tests.js
@@ -1,16 +1,105 @@
 let testCases;
 
-describe("funcName()", function() {
+describe("Parking System 1 (big: 1, medium: 1, small: 0)", function() {
 
-  testCases = {
-    'type-input-here-1': type_output_here_1,
-    'type-input-here-2': type_output_here_2,
-    ...
+  let packingSystem = new ParkingSystem(1, 1 ,0);
+  console.log(packingSystem)
+
+  testCases = [
+    [1, true],
+    [2, true],
+    [3, false],
+    [1, false],
+  ];
+
+  for (const [input, output] of testCases) {
+    it(`${input} → ${output}`, function() {
+      assert.equal(packingSystem.addCar(input), output);
+    });
+  }
+  console.log(packingSystem)
+
+});
+
+describe("Parking System 2 (big: 7, medium: 7, small: 7)", function() {
+
+  let packingSystem = new ParkingSystem(7, 7 ,7);
+  console.log(packingSystem)
+
+  testCases = [
+    [1, true],
+    [1, true],
+    [1, true],
+    [1, true],
+    [1, true],
+    [1, true],
+    [1, true],
+    [1, false],
+
+    [2, true],
+    [2, true],
+    [2, true],
+    [2, true],
+    [2, true],
+    [2, true],
+    [2, true],
+    [2, false],
+
+    [3, true],
+    [3, true],
+    [3, true],
+    [3, true],
+    [3, true],
+    [3, true],
+    [3, true],
+    [3, false],
+  ];
+
+  for (const [input, output] of testCases) {
+    it(`${input} → ${output}`, function() {
+      assert.equal(packingSystem.addCar(input), output);
+    });
   }
 
-  for (const [input, output] of Object.entries(testCases)) {
+});
+
+describe("Parking System 3 (big: 4, medium: 2, small: 0)", function() {
+
+  let packingSystem = new ParkingSystem(4, 2 ,0);
+  console.log(packingSystem)
+
+  testCases = [
+    [1, true],
+    [1, true],
+    [1, true],
+    [1, true],
+    [1, false],
+    [1, false],
+    [1, false],
+    [1, false],
+
+    [2, true],
+    [2, true],
+    [2, false],
+    [2, false],
+    [2, false],
+    [2, false],
+    [2, false],
+    [2, false],
+
+    [3, false],
+    [3, false],
+    [3, false],
+    [3, false],
+    [3, false],
+    [3, false],
+    [3, false],
+    [3, false],
+  ];
+
+  for (const [input, output] of testCases) {
     it(`${input} → ${output}`, function() {
-      assert.equal(funcName(input), output);
+      assert.equal(packingSystem.addCar(input), output);
     });
   }
 
